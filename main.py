698caa9c126994429c48b8c60b95738b2060af31
Taucari <89819413+Taucari@users.noreply.github.com>
2022-06-16 15:34:10 +0100

    Update main.py
    Added comments and debug screen button counter

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import tkinter
 import vlc
 import platform
 
+# This global current_state variable determines which video and how long to wait for each video
 current_state = 0
 
-
+# Just a class to make it easier to define the vlc player in a tkinter Frame easier
 class Screen(tkinter.Frame):
     '''
     Screen widget: Embedded video player from local or youtube
@@ -17,16 +18,18 @@ class Screen(tkinter.Frame):
         self.instance = vlc.Instance()
         self.player = self.instance.media_player_new()
 
-    def GetHandle(self):
-        # Getting frame ID
-        return self.winfo_id()
+# You shouldn't need this stuff
+#    def GetHandle(self):
+#        # Getting frame ID
+#        return self.winfo_id()
 
     def play(self, _source):
         # Function to start player from given source
         media = self.instance.media_new(_source)
         media.get_mrl()
         self.player.set_media(media)
-
+        
+        # When different functions to be used based on when running on Windows vs on Linux
         if platform.system() == 'Windows':
             print("Running on Windows")
             self.player.set_hwnd(self.winfo_id())
@@ -38,55 +41,76 @@ class Screen(tkinter.Frame):
 
 
 def hide_button():
+    # Function to be called when hiding the button as the player appears
     button.place_forget()
 
 
 def return_button():
+    # Function to be called when un-hiding the button as the player disappears
     button.place(anchor="center", relx=0.5, rely=0.5)
 
 
 def hide_player():
+    # Function to hide the player
     player.place_forget()
 
 
 def return_player():
+    # Function to un-hide the player, probably change width and height for different clips (can insert stator and the clip selector dict for control)
     player.place(x=0, y=0, width=1920, height=1080)
 
 
 def button_press(event=None):
+    # Function that is called when the button is pressed
+    # Clip is selected with the stator from the remainder of the division with the clip_selector dict
     global current_state
     clip_selector = {0: {'path': r"clip1.mp4", 'length': 12000},
                      1: {'path': r"clip2.mp4", 'length': 14000},
                      2: {'path': r"clip3.mp4", 'length': 17000}}
     stator = current_state % len(clip_selector)
-
+    print("Button pressed " + str(current_state) + " times.")
+    
+    # Hide button and un-hide player
     hide_button()
     return_player()
+    
+    # Play the right clip
     player.play(clip_selector[stator]['path'])
-
+    
+    # After the clip is finished, hide the player and return the button
     r.after(clip_selector[stator]['length'], hide_player)
     r.after(clip_selector[stator]['length'], return_button)
+    
+    # Increment the counter
     current_state += 1
 
 
 def close(event=None):
+    # Function to be called when 'ESC' button is pressed
     print("Program User Terminated")
     r.destroy()
 
 
-r = tkinter.Tk()
+# Start of the actual configuration of tkinter gui
+r = tkinter.Tk() #root definition
 r.configure(background='black')
 r.resizable(False, False)
 r.attributes("-fullscreen", True)
 r.config(cursor="none")
 
+# Loading the image to be used as a button
 photo = tkinter.PhotoImage(file=r"C:\Users\wangh\PycharmProjects\tkinter_videoplayer\check.png")
 button = tkinter.Label(r, image=photo)
+# Binding the left mouse click on the image as a button
 button.bind('<Button-1>', button_press)
 
+# Binding the escape key to close the program whenever
 r.bind('<Escape>', close)
 
+# Initial creation of a screen object
 player = Screen(r)
+# Button Init
 return_button()
 
+# Start main gui loop
 r.mainloop()
